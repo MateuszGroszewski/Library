f88ff10f5c00f90fae4c4ba73a9881c1896688e8
Mateusz <neonowa.vi@gmail.com>
2024-06-04 17:53:24 +0200

    feat: add create if exist userDB feat: add function addUser

---
+++ src/databases/userDB.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+class UserDB:
+
+    @staticmethod
+    def createUserDB() -> None:
+        userDBConnection = sqlite3.connect("userDB.db")
+        userDBCursor = userDBConnection.cursor()
+        userDBCursor.execute("""CREATE TABLE IF NOT EXISTS users (
+        USERID INTEGER PRIMARY KEY AUTOINCREMENT,
+        NAME TEXT,
+        LASTNAME TEXT,
+        MAIL TEXT,
+        USERNAME TEXT,
+        PASSWORD TEXT
+        )
+        """)
+
+        userDBConnection.commit()
+        userDBConnection.close()
+
+    @staticmethod
+    def addUser(name: str, lastname: str, mail: str, username: str, password: str) -> None:
+        userDBConnection = sqlite3.connect("userDB.db")
+        userDBCursor = userDBConnection.cursor()
+        userDBCursor.execute(f"INSERT INTO users (NAME, LASTNAME, MAIL, USERNAME, PASSWORD) VALUES ('{name}', '{lastname}', '{mail}', '{username}', '{password}')")
+        userDBConnection.commit()
+        userDBConnection.close()
+
+    @staticmethod
+    def editUser(userID: int):
+        raise NotImplemented
+
+    @staticmethod
+    def deleteUser(userID: int):
+        raise NotImplemented
