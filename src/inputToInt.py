ce7fd25a414a9ff17636155621b7c24d81d701d1
Mateusz Groszewski <neonowa.vi@gmail.com>
2024-06-06 22:25:30 +0200

    build: cosmetic changes

--- src/inputToInt.py
+++ src/inputToInt.py
@@ -1,6 +1,6 @@
 def convertInputToInt(entryValue: any) -> int | None:
     """
-    if possible converts a string input into an integer, otherwise returns None
+    if possible converts input into an integer, otherwise returns None
     :param entryValue: string
     :return:  INT or None
     """
