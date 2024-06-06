6f64fbdd29cbe27d68bc0441cd48581a6ec981c5
Mateusz Groszewski <neonowa.vi@gmail.com>
2024-06-06 22:25:24 +0200

    build: cosmetic changes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from src.authentication import Authentication
 from src.databases.userDB import UserDB
 from src.menu import Menu
 
 if __name__ == '__main__':
     UserDB.createUserDB()
-    Menu.showMenu()
-    #Authentication.startScreen()
+    Menu.start()
