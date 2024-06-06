94c9db258909579664abb47079c944e98c2a37bd
Mateusz Groszewski <neonowa.vi@gmail.com>
2024-06-06 22:26:01 +0200

    feat: add login case

--- src/menu.py
+++ src/menu.py
@@ -1,14 +1,34 @@
 from src.inputToInt import convertInputToInt
+from src.authentication import Authentication
 from src.databases.userDB import UserDB
 
 
 class Menu:
 
     @staticmethod
+    def start():
+        print("1. Login")
+        print("2. Register")
+        print("0. Exit")
+        choice = convertInputToInt(input("Enter your choice: "))
+
+        match choice:
+            case 1:
+                print("Login")
+                username = input("Enter your username: ")
+                password = input("Enter your password: ")
+                loggedIn: bool = Authentication.login(username, password)
+                if loggedIn:
+                    Menu.showMenu()
+            case 2:
+                Authentication.register("Anna", "Smith", "anna.smith@email.com", "Anna98", "123")
+
+    @staticmethod
     def showMenu():
         print("Menu")
         print("1. Browse books")
         print("2. Rental history")
+        print("3. My account")
         print("0. Exit")
         choice = convertInputToInt(input("Enter your choice: "))
 
@@ -20,4 +40,6 @@ class Menu:
                 userName = input("Enter your user name: ")
                 password = input("Enter your password: ")
                 UserDB.addUser(name, lastName, mail, userName, password)
-        
+            case 2:
+                name = input("Enter your name: ")
+                UserDB.findUserLoginData(name)
